8aee65a86affa3013e8e3310d4f207449cffe9cd
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2024-05-29 23:11:02 -0230

    added functions in ts

--- dist/hellotype.js
+++ dist/hellotype.js
@@ -2,3 +2,12 @@
 let myMoney = 120;
 myMoney = 40;
 let myName = "hello";
+myName = "Salith Rahman";
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(1, 2);
+let friend = {
+    name: "Salith",
+    salary: 1000000,
+};
--- hellotype.ts
+++ hellotype.ts
@@ -2,3 +2,17 @@ let myMoney: number = 120;
 myMoney = 40;
 let myName: string = "hello";
 myName = "Salith Rahman";
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+add(1, 2);
+
+let friend: {
+  name: string;
+  salary: number;
+} = {
+  name: "Salith",
+  salary: 1000000,
+};
